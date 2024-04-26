aa55b988dc0d906844bcd4a4b339e041cc36a191
Niyush-04 <mauryapradip04@gmail.com>
2024-04-26 23:13:18 +0530

    few changes in UI page

--- app/src/main/java/itm/pbl/cleancity/presentation/VilenessScreen.kt
+++ app/src/main/java/itm/pbl/cleancity/presentation/VilenessScreen.kt
@@ -3,7 +3,10 @@ package itm.pbl.cleancity.presentation
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -11,7 +14,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MyLocation
+import androidx.compose.material.icons.rounded.ThumbDown
+import androidx.compose.material.icons.rounded.ThumbUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +30,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -29,6 +40,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.firestore
+import itm.pbl.cleancity.ui.theme.GreenLightWala
 import itm.pbl.cleancity.ui.theme.backgroundColormain
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -37,19 +49,18 @@ import kotlinx.coroutines.tasks.await
 
 @Composable
 fun VilenessScreen() {
-    Surface (
+    Surface(
         modifier = Modifier.padding(top = 60.dp, bottom = 50.dp),
         color = backgroundColormain
-    ){
+    ) {
         MyCards()
     }
 }
 
 
-
 @SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun MyCards () {
+fun MyCards() {
     val documentKeysState = remember { mutableStateOf<List<String>>(emptyList()) }
 
     GlobalScope.launch(Dispatchers.IO) {
@@ -61,18 +72,22 @@ fun MyCards () {
         documentKeysState.value = documentKeys
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(16.dp)
-        .verticalScroll(state = rememberScrollState())) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .verticalScroll(state = rememberScrollState()),
+    ) {
         documentKeysState.value.forEach { key ->
-            Posts(key = key)
+            CardItem(key = key)
         }
     }
+
 }
 
+
 @Composable
-fun Posts(key: String) {
+fun CardItem(key: String) {
     val db = Firebase.firestore
     val ref = db.collection("vileness").document(key)
 
@@ -89,21 +104,64 @@ fun Posts(key: String) {
     }
 
     Card(
-        modifier = Modifier.fillMaxWidth().background(Color.Transparent).padding(bottom = 16.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Transparent)
+            .padding(bottom = 16.dp),
+        colors = CardDefaults.cardColors(GreenLightWala.copy(0.3f))
 
     ) {
         Image(
             modifier = Modifier
                 .height(250.dp)
                 .fillMaxWidth()
-                .padding(20.dp)
+                .padding(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 10.dp)
                 .clip(RoundedCornerShape(10.dp)),
             painter = rememberAsyncImagePainter(model = imageUrl),
             contentDescription = null,
             contentScale = ContentScale.Crop
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, bottom = 10.dp)
+        ) {
+            Icon(imageVector = Icons.Rounded.MyLocation, contentDescription = "location icon")
+            Text(text = location, modifier = Modifier.padding(start = 10.dp))
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                LinearProgressIndicator(
+                    progress = 0.7f,
+                    color = GreenLightWala,
 
-        Text(text = location, modifier = Modifier.padding(start = 10.dp))
+                    )
+                Icon(imageVector = Icons.Rounded.ThumbUp, contentDescription = null)
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                LinearProgressIndicator(
+                    progress = 0.7f,
+                    color = GreenLightWala,
+
+                    )
+                Icon(imageVector = Icons.Rounded.ThumbDown, contentDescription = null)
+            }
+        }
     }
 }
 
